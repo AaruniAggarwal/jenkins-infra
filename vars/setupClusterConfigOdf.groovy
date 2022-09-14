f00afec9cba39e6ef32e5b25f995e1ee95b37c45
Aaruni Aggarwal <aaruniagg@gmail.com>
2022-09-14 13:24:18 +0530

    updating config file

--- vars/setupClusterConfigOdf.groovy
+++ vars/setupClusterConfigOdf.groovy
@@ -1,35 +1,8 @@
-idef call(String config, String noOfWorkers="2"){
+idef call(String config, String noOfWorkers="3"){
     script{
         //Setup PowerVS cluster config
         //Min config
         if (config == "min") {
-            env.BASTION_MEMORY = "8"
-            env.BASTION_PROCESSORS = ".5"
-            env.BASTION_VCPUS = "2"
-
-            env.BOOTSTRAP_MEMORY = "16"
-            env.BOOTSTRAP_PROCESSORS = ".5"
-            env.BOOTSTRAP_VCPUS = "2"
-
-            env.NUM_OF_MASTERS = "3"
-            env.MASTER_PROCESSORS = ".5"
-            env.MASTER_VCPUS = "1" 
-
-            env.NUM_OF_WORKERS = noOfWorkers
-            env.WORKER_PROCESSORS = ".5"
-            env.WORKER_VCPUS = "1" 
-
-            if (OCP_RELEASE == "4.6" || OCP_RELEASE == "4.7") {
-                env.MASTER_MEMORY = "32"
-                env.WORKER_MEMORY = "32"
-            }
-            else {
-                env.MASTER_MEMORY = "16"
-                env.WORKER_MEMORY = "16"
-            }
-        }
-        //Max Config
-        else{
             env.BASTION_MEMORY = "16"
             env.BASTION_PROCESSORS = "1"
             
@@ -38,10 +11,32 @@ idef call(String config, String noOfWorkers="2"){
             
             env.NUM_OF_MASTERS = "3"
             env.MASTER_PROCESSORS = "1.25"
+            env.MASTER_MEMORY = "32" 
+
+            env.NUM_OF_WORKERS = noOfWorkers
+            env.WORKER_PROCESSORS = "1.5"
+            env.WORKER_MEMORY = "64"
+
+            
+        }
+        //Max Config
+        else{
+            env.BASTION_MEMORY = "16"
+            env.BASTION_PROCESSORS = "1"
+            env.BASTION_VCPUS = "2"
+
+            env.BOOTSTRAP_MEMORY = "16"
+            env.BOOTSTRAP_PROCESSORS = ".5"
+            env.BOOTSTRAP_VCPUS = "2"
+
+            env.NUM_OF_MASTERS = "3"
+            env.MASTER_PROCESSORS = "1.25"
+            env.MASTER_VCPUS = "2"
             env.MASTER_MEMORY = "32"
 
-            env.NUM_OF_WORKERS = 3
+            env.NUM_OF_WORKERS = "4"
             env.WORKER_PROCESSORS = "1.5"
+            env.WORKER_VCPUS = "3" 
             env.WORKER_MEMORY = "64"
         }
     }
