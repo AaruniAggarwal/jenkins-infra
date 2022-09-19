9e992ced9bfbd1f8bd2069b8ffcb160f97b59743
Aaruni Aggarwal <aaruniagg@gmail.com>
2022-09-19 19:19:14 +0530

    correcting minor issue

--- vars/getKeyOdf.groovy
+++ vars/getKeyOdf.groovy
@@ -1,6 +1,6 @@
 def call(){
     withCredentials([file(credentialsId: 'PRIVATE_KEY', variable: 'FILE')]) {
-        sh 'cp $FILE > $RSA_KEY'
+        sh 'cp $FILE $RSA_KEY'
         sh 'chmod 600 $RSA_KEY'
     }
 }
