a7293ec2691f61413e7f319e6ea7025b5c0948c9
Aaruni Aggarwal <aaruniagg@gmail.com>
2022-09-19 19:11:07 +0530

    changing getKey function

--- vars/getKeyOdf.groovy
+++ vars/getKeyOdf.groovy
@@ -1,5 +1,5 @@
 def call(){
-    withCredentials([file(credentialsId: 'PRIVATE_KEY', variable: 'FILE')]) {
+    withCredentials([string(credentialsId: 'PRIVATE_KEY_ODF', variable: 'FILE')]) {
         sh 'echo  $FILE > $RSA_KEY'
         sh 'chmod 0400 $RSA_KEY'
     }
