d89f037ac4553da6c1d6d4564e0272c8f615a4f8
Aaruni Aggarwal <aaruniagg@gmail.com>
2022-09-19 19:16:54 +0530

    using cp instead of echo in getKey function

--- vars/getKeyOdf.groovy
+++ vars/getKeyOdf.groovy
@@ -1,6 +1,6 @@
 def call(){
-    withCredentials([string(credentialsId: 'PRIVATE_KEY_ODF', variable: 'FILE')]) {
-        sh 'echo  $FILE > $RSA_KEY'
-        sh 'chmod 0400 $RSA_KEY'
+    withCredentials([file(credentialsId: 'PRIVATE_KEY', variable: 'FILE')]) {
+        sh 'cp $FILE > $RSA_KEY'
+        sh 'chmod 600 $RSA_KEY'
     }
 }
