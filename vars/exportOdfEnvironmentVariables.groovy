b8d8076672950720649c8b87bd50b70e2d0b0759
Aaruni Aggarwal <aaruniagg@gmail.com>
2022-10-03 14:07:08 +0530

    Adding Jenkinsfile for powerVM odf job

--- vars/exportOdfEnvironmentVariables.groovy
+++ vars/exportOdfEnvironmentVariables.groovy
@@ -4,6 +4,7 @@ def call(){
             echo ""
         }
         try {
+            if ( env.POWERVS == "true" ) {
             sh '''
                echo "export PLATFORM=${PLATFORM}" > odf_vars.sh
                echo "export OCP_VERSION=${OCP_RELEASE}" >> odf_vars.sh
@@ -20,6 +21,26 @@ def call(){
                ssh -o 'StrictHostKeyChecking no' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "echo Deploying ODF version - ${ODF_VERSION}"
                
             '''
+            }
+            else {
+              sh '''
+               echo "export PLATFORM=${PLATFORM}" > odf_vars.sh
+               echo "export PVC_URL=${OS_AUTH_URL}" >> odf_vars.sh
+               echo "export PVC_LOGIN_NAME=${PVC_USERNAME}" >> odf_vars.sh
+               echo "export PVC_LOGIN_PASSWORD=${PVC_PASSWORD}" >> odf_vars.sh
+               echo "export OCP_VERSION=${OCP_RELEASE}" >> odf_vars.sh
+               echo "export OCS_VERSION=${ODF_VERSION}" >> odf_vars.sh
+               echo "export RHID_USERNAME=${REDHAT_USERNAME}" >> odf_vars.sh
+               echo "export RHID_PASSWORD=${REDHAT_PASSWORD}" >> odf_vars.sh
+               echo "export OCS_REGISTRY_IMAGE=quay.io/rhceph-dev/ocs-registry:latest-stable-${ODF_VERSION}" >> odf_vars.sh
+               echo "export VAULT_SUPPORT=${ENABLE_VAULT}" >> odf_vars.sh
+               echo "export FIPS_ENABLEMENT=${ENABLE_FIPS}" >> odf_vars.sh
+               scp -o 'StrictHostKeyChecking no' -i ${WORKSPACE}/deploy/id_rsa odf_vars.sh root@${BASTION_IP}:/root/
+               ssh -o 'StrictHostKeyChecking no' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "chmod +x odf_vars.sh; source odf_vars.sh"
+               ssh -o 'StrictHostKeyChecking no' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "echo Deploying ODF version - ${ODF_VERSION}"
+               
+            '''  
+            }
         }
         catch (err) {
             echo 'Error ! Exporting of environment variables failed!'
